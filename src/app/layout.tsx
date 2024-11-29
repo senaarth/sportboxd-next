a6891cdd082a108b70cfc70efac02ff083f8b790
senaarth <senaarth@gmail.com>
2024-11-29 10:53:22 -0300

    feat: hotjar

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -44,6 +44,23 @@ export default function RootLayout({
           crossOrigin="anonymous"
           strategy="afterInteractive"
         />
+        {/* <!-- Hotjar Tracking Code for Site 5218288 (nome ausente) --> */}
+        <Script
+          id="hotjar-script"
+          strategy="afterInteractive"
+          dangerouslySetInnerHTML={{
+            __html: `
+            (function(h,o,t,j,a,r){
+              h.hj=h.hj||function(){(h.hj.q=h.hj.q||[]).push(arguments)};
+              h._hjSettings={hjid:5218288,hjsv:6};
+              a=o.getElementsByTagName('head')[0];
+              r=o.createElement('script');r.async=1;
+              r.src=t+h._hjSettings.hjid+j+h._hjSettings.hjsv;
+              a.appendChild(r);
+            })(window,document,'https://static.hotjar.com/c/hotjar-','.js?sv=');
+          `,
+          }}
+        />
         <meta name="google-adsense-account" content="ca-pub-2303783235779047" />
       </head>
       <body className="antialiased font-inter flex flex-col items-center justify-start min-h-svh bg-neutral-950">
